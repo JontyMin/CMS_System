3be2f13afcb7ae82f8406f1f5edcedf88f9e0888
jackmaerk <51729732+jackmaerk@users.noreply.github.com>
2019-10-28 17:24:34 +0800

    前台界面搭建

---
+++ CMS_System/Controllers/IndexController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Model;
+using Dal;
+namespace CMS_System.Controllers
+{
+    public class IndexController : Controller ,IDisposable
+    {
+		IndexDal db = new IndexDal();
+        // GET: Index
+        public ActionResult Index()
+        {
+			var ls = db.GetCMS_ArticlesByCidTopN(1,7);//网站公告
+			ViewBag.Center = db.GetCMS_ArticlesByCidTopN(2, 6);//产品中心
+			ViewBag.service= db.GetCMS_ArticlesByCidTopN(3, 6);//定制服务
+			ViewBag.Case = db.GetCMS_ArticlesByCidTopN(4, 6);//成功案例
+			return View(ls);
+        }
+
+		public ActionResult Page(int aid)
+		{
+			return View();
+		}
+
+		public ActionResult List(int cid)
+		{
+			return View();
+		}
+		protected override void Dispose(bool disposing)
+		{
+			db.Dispose();
+			base.Dispose(disposing);
+		}
+	}
+}
---
+++ Dal/IndexDal.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+namespace Dal
+{
+	
+	public  class IndexDal:IDisposable
+	{
+		db_CMS db = new db_CMS();
+
+		/// <summary>
+		/// 列表查询
+		/// </summary>
+		/// <returns></returns>
+		public List<Model.CMS_Category> GetCMS_Categories()
+		{
+			return db.CMS_Category.OrderBy(c=>c.navorder).ToList();
+		}
+
+
+		/// <summary>
+		/// 根据文章栏目提取热度最高文章
+		/// </summary>
+		/// <param name="cid">栏目id</param>
+		/// <param name="top">条数</param>
+		/// <returns></returns>
+		public List<CMS_Article> GetCMS_ArticlesByCidTopN(int cid, int top)
+		{
+
+			return db.CMS_Article.OrderByDescending(c => c.hits)//根据点击率倒序查询
+				.Where(c => c.state == 2 && c.cid==cid)//状态为发布
+				.Take(top)//提取
+				.ToList();
+		}
+
+
+		/// <summary>
+		/// 资源释放
+		/// </summary>
+		public void Dispose()
+		{
+			db.Dispose();
+
+		}
+	}
+}
